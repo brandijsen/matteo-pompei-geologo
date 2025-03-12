b72a77f61e3ef640635ca9af178fabebbdd45465
Andrea Brandetti <andrebrandet93@gmail.com>
2025-03-12 17:48:53 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/matteo-pompei-geologo/', // Sostituisci con il nome del tuo repository
 
-  plugins: [react()],
+  plugins: [react(),     
+    tailwindcss(),
+  ],
 })
