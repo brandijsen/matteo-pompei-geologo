e733cc7eeb38469922f19f43e1d541d11fbaceea
Andrea Brandetti <andrebrandet93@gmail.com>
2025-03-12 18:02:52 +0100

    project cloned

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Services from './pages/Services/Services'
+import Service from './pages/Service/Service'
+import Projects from './pages/Projects/Projects'
+import Project from "./pages/Project/Project";
+
+import Contact from './pages/Contact/Contact'
+
 import './App.css'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-    <h1 className='text-red-500'> ciao</h1>
-    </>
-  )
-}
+    <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/service/:serviceName" element={<Service />} />
 
-export default App
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects/:projectId" element={<Project />} />
+
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+    </Router>
+  );
+};
+
+export default App;
